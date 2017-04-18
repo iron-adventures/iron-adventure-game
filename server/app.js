e562539fb057a39cce59ca462aa37c3303de5720
Nadia Mughal <nmughal14@gmail.com>
2017-04-18 17:25:29 -0400

    Corrected the express static path and added the player route

--- server/app.js
+++ server/app.js
@@ -7,17 +7,11 @@ let app = express();
 
 console.log('Database location', process.env.MY_DB_LOCATION);
 
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../client/src'));
 
 app.use(bodyParser.json());
 
-app.get('/', function showHomePage(request, response, next) {
-  console.log(request.url);
-
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html');
-  response.end('<h1>Hello World</h1>');
-});
+app.use('/api/players', require('./routes/player.routes.js'));
 
 app.listen(3000, function doSomethingServer() {
   console.log('The server is now up');
