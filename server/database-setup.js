54648475a2f8c77f008c90712e49caf120b46276
Nadia Mughal <nmughal14@gmail.com>
2017-04-19 09:48:49 -0400

    Changed the environment variable name

--- server/database-setup.js
+++ server/database-setup.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-if (!process.env.MY_DB_LOCATION) {
+if (!process.env.IRON_ADVENTURE_MONGO_DB_URL) {
   console.log('No database detected');
   process.exit();
 }
 
-mongoose.connect(process.env.MY_DB_LOCATION);
+console.log('Database location', process.env.IRON_ADVENTURE_MONGO_DB_URL);
+mongoose.connect(process.env.IRON_ADVENTURE_MONGO_DB_URL);
 
 mongoose.connection.on('error', function handleDBErrors(err) {
   console.log('DB Error', err);
