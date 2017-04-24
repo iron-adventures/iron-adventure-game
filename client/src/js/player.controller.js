7d5d8d18ba1b00e1f6d65bafbeafc9be1fdcfb32
Nadia Mughal <nmughal14@gmail.com>
2017-04-24 14:54:27 -0400

    Initialized storePlayer object as falsey and connected the loginPlayer function to player service

--- client/src/js/player.controller.js
+++ client/src/js/player.controller.js
@@ -15,16 +15,30 @@
     vm.playerInfo = {};
     vm.hasError = false;
     vm.message = null;
+    vm.storedPlayer = false; // initialize as falsey
 
     console.log('Is the PlayerController running?');
 
     /**
-     * Player input
-     * @param  {Object}   playerInfo  Player info
+     * Player input (from client side)
+     * @param  {Object}   playerInfo  Object containing the player's info
      * @return {Promise}
      */
     vm.input = function input(playerInfo) {
       console.log('makin a player', playerInfo);
+
+
+      PlayerService.loginPlayer(playerInfo)
+        .then(function handleResponseData(responseData) {
+          vm.storedPlayer = responseData;
+          console.log('responseData is', responseData);
+        })
+        .catch(function handleErrors(errResponse) {
+          console.warn(errResponse);
+          vm.hasError = true;
+        });
+
+
     };
 
   }
