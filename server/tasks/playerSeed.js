2a5fa955434c34ade49374425034584caa8021f1
David Steed <davidasteed@gmail.com>
2017-04-24 19:35:02 -0400

    Continued work on scene advance logic

--- server/tasks/playerSeed.js
+++ server/tasks/playerSeed.js
@@ -1,2 +1,12 @@
 require('../database-setup.js');
 const Player = require('../models/Player.model.js');
+
+new Player(
+  {
+    playerName: 'dsteed',
+    playerEmail: 'davidS@world.com',
+    playerScore: 0,
+    playerScene: ''
+  }).save().then(function done() {
+    process.exit();
+  });
