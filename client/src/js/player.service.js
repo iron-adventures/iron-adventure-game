47f5f8ede209989a8d97e888ec3fa630b9ac7f45
Nadia Mughal <nmughal14@gmail.com>
2017-04-25 16:51:05 -0400

    Created a playerEmail variable to store player email in localStorage and added functions to retrieve email and logout

--- client/src/js/player.service.js
+++ client/src/js/player.service.js
@@ -2,23 +2,23 @@
   'use strict';
 
   angular.module('adventure')
-    .factory('PlayerService', PlayerService);
+  .factory('PlayerService', PlayerService);
 
   PlayerService.$inject = ['$http'];
 
   /**
-   * Creates a new PlayerService singleton
-   * @param {Function} $http  The service for making ajax calls
-   * @return {Object}         The service's API methods
-   */
+  * Creates a new PlayerService singleton
+  * @param  {Function} $http  The service for making ajax calls
+  * @return {Object}          The service's API methods
+  */
   function PlayerService($http) {
+    let playerEmail = localStorage.getItem('email');
 
     /**
-     * Player inputs data
-     * @param {String} playerName
-     * @param {String} playerEmail
-     * @return {Promise}
-     */
+    * Log in player with the API given user provided info
+    * @param   {Object}    loginDetails   Object containing player name and email
+    * @return  {Promise}
+    */
     function loginPlayer(loginDetails) {
       if (typeof(loginDetails.playerName) !== 'string' || typeof(loginDetails.playerEmail) !== 'string') {
         return Promise.reject('Invalid information');
@@ -35,14 +35,37 @@
           playerEmail: loginDetails.playerEmail
         })
       })
-        .then(function handleResponse(responseObj) {
-          console.log('Response object', responseObj.data);
-          return(responseObj.data);
-        });
+      .then(function handleResponse(responseObj) {
+        console.log('Response object', responseObj.data);
+        playerEmail = responseObj.data.thePlayerAdded.playerEmail;
+        localStorage.setItem('email', playerEmail);
+        return(responseObj.data);
+      });
 
     }
+
+    /**
+    * Returns the player's email
+    * @return {String} The unique email for a player
+    */
+    function getEmail() {
+      return email;
+    }
+
+    /**
+    * Logs a player out of the system
+    * @return {void}
+    */
+    function logout() {
+      playerEmail = null;
+      localStorage.removeItem('email');
+    }
+
+
     return {
-      loginPlayer: loginPlayer
+      loginPlayer: loginPlayer,
+      getEmail: getEmail,
+      logout: logout
     };
 
   }
