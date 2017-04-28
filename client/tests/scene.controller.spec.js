1e1d24e43e4ffc9d46adbfb81716b090693697e1
David Steed <davidasteed@gmail.com>
2017-04-28 15:29:58 -0400

    Added test for getScene() that validates for an argument with value of undefined

--- client/tests/scene.controller.spec.js
+++ client/tests/scene.controller.spec.js
@@ -24,29 +24,49 @@
             sceneText:
             'You\'ve been stuck on a homework problem for several hours',
             sceneChoices:
-             [
-               { _id: '58ffe14978feb61989d68e0a',
+             [ { _id: '58ffe14978feb61989d68e0a',
                  choiceScore: 5,
                  choiceText:
                  'I own this problem.  Take whatever time is needed.',
-                 choiceIcon: 'glyphicon glyphicon-thumbs-up'
-               },
+                 choiceIcon: 'glyphicon glyphicon-thumbs-up' },
                { _id: '58ffe14978feb61989d68e09',
                  choiceScore: 5,
                  choiceText:
                  'Work on the problem for a final, set period of time.',
-                 choiceIcon: 'glyphicon glyphicon-flash'
-               },
+                 choiceIcon: 'glyphicon glyphicon-flash' },
                { _id: '58ffe14978feb61989d68e08',
                  choiceScore: 10,
                  choiceText:
                  'Ask a fellow student or instructor for assistance.',
-                 choiceIcon: 'glyphicon glyphicon-question-sign'
-               }
-             ]
-           });
+                 choiceIcon: 'glyphicon glyphicon-question-sign' } ] });
+                };
+
+      mockSceneService.getScene = function getScene(inputEmail) {
+        mockSceneService.getScene.numTimesCalled++;
+        return Promise.resolve(
+          { id: '58ffe14978feb61989d68e07',
+            sceneImage: 'images/3078856253_aa1e08579c_z.jpg',
+            sceneText:
+            'You\'ve been stuck on a homework problem for several hours',
+            sceneChoices:
+             [ { _id: '58ffe14978feb61989d68e0a',
+                 choiceScore: 5,
+                 choiceText:
+                 'I own this problem.  Take whatever time is needed.',
+                 choiceIcon: 'glyphicon glyphicon-thumbs-up' },
+               { _id: '58ffe14978feb61989d68e09',
+                 choiceScore: 5,
+                 choiceText:
+                 'Work on the problem for a final, set period of time.',
+                 choiceIcon: 'glyphicon glyphicon-flash' },
+               { _id: '58ffe14978feb61989d68e08',
+                 choiceScore: 10,
+                 choiceText:
+                 'Ask a fellow student or instructor for assistance.',
+                 choiceIcon: 'glyphicon glyphicon-question-sign' } ] });
       };
 
+      mockSceneService.getScene.numTimesCalled = 0;
       mockSceneService.loadScene.numTimesCalled = 0;
 
       // instantiate the mock controller
@@ -59,23 +79,27 @@
       // localStorage.removeItem('email');
     }));
 
-    // it('should add 2+2', function() {
-    //   expect(2+2).to.equal(4);
-    // });
-
     it('should fail to loadScene() if input ID is invalid', function () {
-
       // attempt to pass inputID value of undefined
       let undefinedID;
+      let testEmail = 'davidS@dude.com';
 
       SceneController.loadScene(
         undefinedID,
         'I own this problem.  Take whatever time is needed.',
-        'davidS@dude.com');
+        testEmail);
 
       expect(mockSceneService.loadScene.numTimesCalled).to.equal(0);
     });
 
+    it('should not run getScene() if inputEmail is undefined', function () {
+      // attempt to pass undefined
+      let undefinedID;
+      let testEmail = 'davidS@dude.com';
+
+      SceneController.getScene(undefinedID);
+      expect(mockSceneService.getScene.numTimesCalled).to.equal(0);
+    });
   });
 
 }());
