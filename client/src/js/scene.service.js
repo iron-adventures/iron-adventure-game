a71f6c3623d63fe5f78be4b7d8277fd54684599e
David Steed <davidasteed@gmail.com>
2017-04-27 21:08:22 -0400

    Function getScene() created with basic functionality with test data used

--- client/src/js/scene.service.js
+++ client/src/js/scene.service.js
@@ -12,6 +12,25 @@
    */
   function SceneService($http) {
     /**
+     * Function getScene() returns current scene for a player
+     * @param  {String} inputEmail Player email
+     * @return {Promise}
+     */
+    function getScene(inputEmail) {
+      return $http({
+        url: '/api/scenes/' + inputEmail,
+        method: 'get',
+        header: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(function handleResponse(responseObj) {
+        console.log('service responseObj is: ', responseObj);
+        return responseObj.data;
+      });
+    }
+
+    /**
      * Function loadScene() advances to the next scene
      * @param  {String} inputId    Current scene
      * @param  {String} inputText  Player choice
@@ -31,13 +50,14 @@
           inputEmail: inputEmail
         })
       })
-        .then(function handleResponse(responseObj) {
-          console.log('service responseObj is: ', responseObj);
-          return responseObj.data;
-        });
+      .then(function handleResponse(responseObj) {
+        console.log('service responseObj is: ', responseObj);
+        return responseObj.data;
+      });
     }
 
     return {
+      getScene: getScene,
       loadScene: loadScene
     };
   }
